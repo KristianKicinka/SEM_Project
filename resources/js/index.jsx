401b43cb41d61e512baf93851b1dc08b0285a1d0
KristianKicinka <kristian.kicinka@gmail.com>
2022-09-22 19:32:49 +0200

    after create Application component and show it

---
+++ resources/js/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Application from './components/Application';
+
+if (document.getElementById('Application')) {
+    ReactDOM.render(<Application />, document.getElementById('Application'));
+}
