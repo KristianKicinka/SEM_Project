b7b98784f2a2e11abc251e3a3964387b755799bf
KristianKicinka <kristian.kicinka@gmail.com>
2023-03-10 21:16:06 +0100

    inser pcap file creating

--- app/Http/Controllers/HashController.php
+++ app/Http/Controllers/HashController.php
@@ -18,35 +18,48 @@ class HashController extends Controller {
     private string $app_package_name;
     private string $app_version_name;
 
-    public function createPcapFile(){
+    public function createPcapFile($apk_name, $apk_path){
 
-        $filter = $this->createFilter();
-        $pcap_out_name = "messenger";
-        $pcap_out_path = "../../../scripts/pcap/".$pcap_out_name;
-        $tshark_command = 'tshark -F pcap -Y "'.$filter.'" -w '.$pcap_out_path;
+        $package_name = trim($this->getAppPackageName($apk_path));
+        $file_name = str_replace('.apk', '.pcap', $apk_name);
+        $pcap_out_path = "./storage/pcaps/".$file_name;
+
+        $pcap_process = new Process(['tshark','-i','en0','-F','pcap','-w',$pcap_out_path]);
+        $pcap_process->start();
+
+        $this->runAppOnEmulator($package_name);
+        sleep(10);
+        $this->closeAppOnEmulator($package_name);
+
+        $pcap_process->stop();
+
+        $print = $this->applyPcapFilter($pcap_out_path);
+
+        return $print;
     }
 
     public function createHash(Request $request){
 
-        $apk_path = "./storage/uploads/apk/".$request->get('file_name');
+        $file_name = $request->get('file_name');
+        $apk_path = "./storage/uploads/apk/".$file_name;
 
         $package_name = trim($this->getAppPackageName($apk_path));
         $version_name = trim($this->getAppVersionName($apk_path));
 
-        //$print = $this->installAppOnEmulator($apk_path);
+        $this->installAppOnEmulator($apk_path);
 
-        $print = $this->uninstallAppOnEmulator($package_name);
+        $print = $this->createPcapFile($file_name, $apk_path);
 
-        //$this->createPcapFile();
-        error_log("Filter created!");
+        $this->uninstallAppOnEmulator($package_name);
 
+    
         return response()->json($print);
 
     }
 
     private function createFilter(): string {
 
-        $filter = " tls.handshake.type==1 && tcp && !(";
+        $filter = "tls.handshake.type==1 && tcp && !(";
         $index = 0;
         foreach ($this->ip_black_list as $ip){
             $filter = $filter." ip.dst==".$ip;
@@ -58,22 +71,34 @@ class HashController extends Controller {
         return $filter.")";
     }
 
+    private function applyPcapFilter($file_path){
+
+        $filter = $this->createFilter();
+        $command = 'tshark -r '.$file_path.' -Y "'.$filter.'" -w '.$file_path;
+        $process = Process::fromShellCommandline($command);
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            return response()->json('Apply pcap filter failed!');
+        }
+
+    }
+
     private function runAppOnEmulator($package_name){
 
-        $command = 'adb shell am start -n '.$package_name.'/'.$package_name.'.MainActivity';
+        $command = 'adb shell monkey -p '.$package_name.' -c android.intent.category.LAUNCHER 1';
 
-        $process = new Process([$command]);
+        $process = Process::fromShellCommandline($command);
         $process->run();
 
         if (!$process->isSuccessful()) {
             return response()->json('App installing error!');
         }
-
     }
 
     private function closeAppOnEmulator($package_name){
 
-        $command = 'adb shell am force-stop '.$package_name;
+        $command = 'adb shell pm clear '.$package_name;
 
         $stop_app_process = Process::fromShellCommandline($command);
         $stop_app_process->run();
@@ -85,6 +110,16 @@ class HashController extends Controller {
 
         if (!$process->isSuccessful()) {
             return response()->json('App installing error!');
+        }
+
+    }
+
+    private function uninstallAppOnEmulator($package_name){
+        $process = new Process(['adb', 'uninstall', $package_name]);
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            return response()->json('App uninstalling error!');
         }
 
     }
@@ -115,18 +150,6 @@ class HashController extends Controller {
         }
         
         return $process->getOutput();
-        
     }
-
-    private function uninstallAppOnEmulator($package_name){
-        $process = new Process(['adb', 'uninstall', $package_name]);
-        $process->run();
-
-        if (!$process->isSuccessful()) {
-            return response()->json('App uninstalling error!');
-        }
-
-    }
-
 
 }
