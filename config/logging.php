af87875a4c66db17da21e1fc88cd1b2c95fcf66e
KristianKicinka <kristian.kicinka@gmail.com>
2023-03-21 11:48:57 +0100

    insert new logging channel for developing

--- config/logging.php
+++ config/logging.php
@@ -117,6 +117,12 @@ return [
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
+
+        'devlog' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/devlog.log'),
+            'level' => 'info',
+        ],
     ],
 
 ];
