cf118b308b1c9b2b898748d8f6baa0f7a9c1dc23
Kristián Kičinka <kristian.kicinka@gmail.com>
2023-04-04 20:29:31 +0200

    feat: rename hash gen script and insert new hash type generator

--- scripts/JA3_JA3S_hash_generator.py
+++ scripts/JA3_JA3S_hash_generator.py
@@ -9,8 +9,8 @@ import os
 tls_supported_groups = []
 tls_ecf = []
 
-JA3_strings = []
-JA3_hashes = []
+hash_strings = []
+hashes = []
 
 
 def process_ciphers(message):
@@ -52,7 +52,7 @@ def add_to_string(full_string, items):
     return full_string
 
 
-def create_ja3_string(version, ciphers, extensions):
+def create_JA3_string(version, ciphers, extensions):
     full_string = "" + str(version) + ","
     full_string = add_to_string(full_string, ciphers) + ","
     full_string = add_to_string(full_string, extensions) + ","
@@ -60,9 +60,15 @@ def create_ja3_string(version, ciphers, extensions):
     full_string = add_to_string(full_string, tls_ecf)
     return full_string
 
+def create_JA3S_string(version, ciphers, extensions):
+    full_string = "" + str(version) + ","
+    full_string = add_to_string(full_string, ciphers) + ","
+    full_string = add_to_string(full_string, extensions)
+    return full_string
 
-def create_ja3_hash(ja3_string):
-    result = hashlib.md5(ja3_string.encode())
+
+def create_hash(hash_string):
+    result = hashlib.md5(hash_string.encode())
     return result.hexdigest()
 
 
@@ -79,29 +85,34 @@ def print_values(packet_id, version, ciphers, extensions, full_string, ja3_hash)
 
 
 
-
 if __name__ == '__main__':
     load_layer('tls')
-    #packets = pyshark.LiveCapture('eth0', bpf_filter='tcp port 80')
+
     scapy_cap = rdpcap(sys.argv[1])
+    hash_type = sys.argv[2]
+
     packet_count = 1
     for packet in scapy_cap:
         version = process_version(packet[TLS].msg[0])
         ciphers = process_ciphers(packet[TLS].msg[0])
         extensions = process_extensions(packet[TLS].msg[0])
 
-        full_string = create_ja3_string(version, ciphers, extensions)
-        ja3_hash = create_ja3_hash(full_string)
+        if(hash_type == "JA3"):
+            full_string = create_JA3_string(version, ciphers, extensions)
+            final_hash = create_hash(full_string)
+        elif(hash_type == "JA3S"):
+            full_string = create_JA3S_string(version, ciphers, extensions)
+            final_hash = create_hash(full_string)
 
-        JA3_strings.append(full_string)
-        JA3_hashes.append(ja3_hash)
+        hash_strings.append(full_string)
+        hashes.append(final_hash)
 
         #print_values(packet_count, version, ciphers, extensions, full_string, ja3_hash)
         packet_count += 1
 
-    final_JA3_list = list(dict.fromkeys(JA3_hashes))
+    final_hash_list = list(dict.fromkeys(hashes))
 
     #print()
     #print('JA3 hash list :')
-    print(final_JA3_list)
+    print(final_hash_list)
     #print()
