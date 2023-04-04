b9fde4571739e0d3806c992aa2a8dc0f6c190abc
Kristián Kičinka <kristian.kicinka@gmail.com>
2023-04-04 22:48:07 +0200

    feat: insert api page template

--- resources/js/components/web/APIPage.jsx
+++ resources/js/components/web/APIPage.jsx
@@ -8,8 +8,26 @@ import Navbar from './partials/Navbar';
 const APIPage = () => {
 
     return (
-        <div className='APIPage'>
+        <div className='APIPage bg-primary bg-gradient pt-5 vh-100'>
             <Navbar/>
+            <div className="container pt-5">
+                <div className="row">
+                    <div className="card bg-white text-dark p-3">
+                        <div className="card-body">
+                            <h5 className="card-title">API overview</h5>
+                            <p className="card-text">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Iste inventore adipisci eligendi nulla necessitatibus delectus
+                                in nam iusto recusandae pariatur, natus eaque quas,
+                                consectetur maxime laudantium fugit eos incidunt voluptatibus.
+                            </p>
+                            <a href="/" className="btn btn-search text-light">
+                                Main page
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
