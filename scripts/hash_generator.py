6e9424f31be4784d7c069093fcf27b9a701d178f
KristianKicinka <kristian.kicinka@gmail.com>
2024-02-23 21:18:34 +0100

    fix: remove prints from hash generation

--- scripts/hash_generator.py
+++ scripts/hash_generator.py
@@ -118,7 +118,7 @@ def get_ec_point_formats(packet):
             if ec_point_formats_field:
                 for format_code in ec_point_formats_field:
                     ec_point_formats.append(format_code)
-                    
+
     return ec_point_formats
 
 # Get SNI
@@ -217,17 +217,17 @@ if __name__ == '__main__':
 
         #packet.show()
 
-        print("################")
-        print(f"IP SRC : {packet[IP].src}")
-        print(f"IP DST : {packet[IP].dst}")
-        print(f"TLS Version : {version}")
-        print(f"TLS Ciphers : {ciphers}")
-        print(f"TLS Extensions : {extensions}")
-        print(f"TLS Supported groups : {supported_groups}")
-        print(f"TLS EC Point format : {point_format}")
-        print(f"JA3 Full string : {full_string}")
-        print(f"JA3 Hash : {final_hash}")
-        print("################")
+        #print("################")
+        #print(f"IP SRC : {packet[IP].src}")
+        #print(f"IP DST : {packet[IP].dst}")
+        #print(f"TLS Version : {version}")
+        #print(f"TLS Ciphers : {ciphers}")
+        #print(f"TLS Extensions : {extensions}")
+        #print(f"TLS Supported groups : {supported_groups}")
+        #print(f"TLS EC Point format : {point_format}")
+        #print(f"JA3 Full string : {full_string}")
+        #print(f"JA3 Hash : {final_hash}")
+        #print("################")
 
         packet_count += 1
     
