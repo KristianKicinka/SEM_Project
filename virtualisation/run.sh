9f9a36e2a06da3f2fbcc36d67b00969a9bca04d9
KristianKicinka <kristian.kicinka@gmail.com>
2024-04-28 10:19:43 +0200

    feat: repair dockerfiles

--- virtualisation/run.sh
+++ virtualisation/run.sh
@@ -4,11 +4,13 @@
 echo "Starting Docker Compose..."
 docker-compose up -d
 
-echo "Creating docker network interfaces..."
-
 echo "Creating database structure..."
+docker exec -it hashapp_web bash -c "sudo -u www-data bash -c \"php artisan migrate;\""
+docker exec -it hashapp_web bash -c "sudo -u www-data bash -c \"php artisan migrate:fresh;\""
+docker exec -it hashapp_web bash -c "sudo -u www-data bash -c \"php artisan db:seed --class=UserSeeder\""
+docker exec -it hashapp_web bash -c "sudo -u www-data bash -c \"php artisan db:seed --class=EmulatorSeeder\""
 
 echo "Importing data to database..."
 
 # Create network interfaces
-
+#RUN mysql -u root -p root sem_project < ./installationFiles/sem_project_db_data.sql
