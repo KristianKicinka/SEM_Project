db66d02d69742ddc1a185383d521f72c000ee2f8
Kristián Kičinka <kristian.kicinka@gmail.com>
2025-11-01 22:31:38 +0100

    fix: repair vite config

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -17,7 +17,30 @@
 
     <!-- Scripts -->
     <script src="https://kit.fontawesome.com/a2ea7766e8.js" crossorigin="anonymous"></script>
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    
+    @if(app()->environment('local'))
+        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @else
+        @php
+            $manifestPath = public_path('build/manifest.json');
+            if (file_exists($manifestPath)) {
+                $manifest = json_decode(file_get_contents($manifestPath), true);
+                $appJs = $manifest['resources/js/app.js'] ?? null;
+                $appScss = $manifest['resources/sass/app.scss'] ?? null;
+            }
+        @endphp
+        @if(isset($appJs['css']))
+            @foreach($appJs['css'] as $css)
+                <link rel="stylesheet" href="{{ asset('build/' . $css) }}">
+            @endforeach
+        @endif
+        @if(isset($appScss['file']))
+            <link rel="stylesheet" href="{{ asset('build/' . $appScss['file']) }}">
+        @endif
+        @if(isset($appJs['file']))
+            <script type="module" src="{{ asset('build/' . $appJs['file']) }}"></script>
+        @endif
+    @endif
 </head>
 <body>
     <div id="Application">
