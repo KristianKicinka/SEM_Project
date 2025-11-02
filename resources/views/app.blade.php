7224858b712161978f1dd64b5594f62dbea8848d
Kristián Kičinka <kristian.kicinka@gmail.com>
2025-11-02 10:45:15 +0100

    fix: remove blade directives

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,38 +16,35 @@
     <!-- Scripts -->
     <script src="https://kit.fontawesome.com/a2ea7766e8.js" crossorigin="anonymous"></script>
     
-    @if(Vite::isRunningHot())
-        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-    @else
-        @php
-            $manifestPath = public_path('build/manifest.json');
-            if (file_exists($manifestPath)) {
-                $manifestContent = file_get_contents($manifestPath);
-                $manifest = json_decode($manifestContent, true);
+    @php
+        // Always load from manifest - this works in all environments (local, dev, production)
+        $manifestPath = public_path('build/manifest.json');
+        if (file_exists($manifestPath)) {
+            $manifestContent = file_get_contents($manifestPath);
+            $manifest = json_decode($manifestContent, true);
+            
+            if ($manifest && isset($manifest['resources/js/app.js'])) {
+                $appJs = $manifest['resources/js/app.js'];
                 
-                if ($manifest && isset($manifest['resources/js/app.js'])) {
-                    $appJs = $manifest['resources/js/app.js'];
-                    
-                    // CSS from app.js
-                    if (!empty($appJs['css']) && is_array($appJs['css'])) {
-                        foreach ($appJs['css'] as $css) {
-                            echo '<link rel="stylesheet" href="' . asset('build/' . $css) . '">' . "\n    ";
-                        }
-                    }
-                    
-                    // app.scss CSS
-                    if (isset($manifest['resources/sass/app.scss']['file'])) {
-                        echo '<link rel="stylesheet" href="' . asset('build/' . $manifest['resources/sass/app.scss']['file']) . '">' . "\n    ";
-                    }
-                    
-                    // app.js
-                    if (!empty($appJs['file'])) {
-                        echo '<script type="module" src="' . asset('build/' . $appJs['file']) . '"></script>' . "\n";
+                // CSS from app.js
+                if (!empty($appJs['css']) && is_array($appJs['css'])) {
+                    foreach ($appJs['css'] as $css) {
+                        echo '<link rel="stylesheet" href="' . asset('build/' . $css) . '">' . "\n    ";
                     }
                 }
+                
+                // app.scss CSS
+                if (isset($manifest['resources/sass/app.scss']['file'])) {
+                    echo '<link rel="stylesheet" href="' . asset('build/' . $manifest['resources/sass/app.scss']['file']) . '">' . "\n    ";
+                }
+                
+                // app.js
+                if (!empty($appJs['file'])) {
+                    echo '<script type="module" src="' . asset('build/' . $appJs['file']) . '"></script>' . "\n";
+                }
             }
-        @endphp
-    @endif
+        }
+    @endphp
 </head>
 <body>
     <div id="Application">
